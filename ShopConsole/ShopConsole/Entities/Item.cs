afa3c757a46a41607c79d93445c9ef3ca6d94f2f
Greta <grigaityte.greta@gmail.com>
2022-11-08 17:17:49 +0200

    Data files updated

---
+++ ShopConsole/ShopConsole/Entities/Item.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopConsole.Entities
+{
+    public class Item
+    {
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public string Barcode { get; set; }
+        public double Weight { get; set; }
+    }
+}
